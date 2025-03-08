ffa087dd527405a385d20e507a95f45db049cbc9
sergi <sergio_socha@yahoo.com>
2025-03-08 07:55:55 -0500

    first commit

---
+++ src/test/java/com/patrones/notify/NotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrones.notify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
