41faf6d0a1e71640047abe03f1fb007a09abd754
Cristian Soto <cristanchos2003@gmail.com>
2025-03-08 09:08:16 -0500

    Corrige State

--- src/main/java/com/patrones/notify/State.java
+++ src/main/java/com/patrones/notify/State.java
@@ -1,19 +1,16 @@
 package com.patrones.notify;
-
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClient;
 
-public abstract class Steate{
+public abstract State {
+
     private final Notifier notifier;
     private final RestClient restClient;
     private String urlProveedor;
+    private String nombreProveedor;
 
     public State(Notifier notifier){
-        this.notifier = notifier;
+
     }
 }
-
-
-
-
