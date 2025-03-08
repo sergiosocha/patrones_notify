0256624b9fd1e0bc0ac5bfb93593864a7d8b3fc5
Cristian Soto <cristanchos2003@gmail.com>
2025-03-08 08:45:49 -0500

    Agrega State

---
+++ src/main/java/com/patrones/notify/State.java
@@ -0,0 +1,24 @@
+package com.patrones.notify;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
+
+public class State {
+
+    public abstract class Steate{
+        private final Notifier notifier;
+        private final RestClient restClient;
+        private String urlProveedor;
+
+
+    }
+
+    public State(Notifier notifier){
+        this.notifier = notifier;
+    }
+
+    public void setNombreProveedor(String urlProveedor){
+
+    }
+}
